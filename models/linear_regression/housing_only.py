07775ae013ef58a1c34d9d66376f32c502585d8b
Kenneth Christensen <1kennethac@gmail.com>
2020-03-25 14:23:40 -0600

    Add box and whiskers to RMSE search plot.

--- models/linear_regression/housing_only.py
+++ models/linear_regression/housing_only.py
@@ -26,33 +26,30 @@ def regress_on_data(data:pd.DataFrame, degree:int):
 def regress_on_all(degree:int):
     data = hd(["15-0000"])
 
-    total = 0
-    count = 0
-
+    ind = []
     for i, city in enumerate(data.iterate_areas()):
         if len(city[1]) < 5:
             continue
 
-        total += regress_on_data(city[1], degree)**2
-        count += 1
-        if i % 25 == 0:
-            rmse = (total / count)**0.5
-            print(f"RMSE so far: {rmse}")
-    
-    rmse = (total / count)**0.5
-    print(f"RMSE: {rmse}")
-    return rmse
+        ind.append(regress_on_data(city[1], degree))
+
+    squares = np.square(np.array(ind))
+    mean = np.mean(squares)
+    rmse = np.sqrt(mean)
+    return rmse, ind
 
 def find_best_degree():
-    results = [ regress_on_all(d) for d in range(1, 6) ]
+    results, actuals = zip(*[ regress_on_all(d) for d in range(1, 6) ])
 
     print(f"Best degree: {np.argmin(results) + 1} with RMSE {min(results)}")
 
-    plt.plot(results)
-    plt.xticks(range(0, 5), range(1, 6))
+    plt.boxplot(np.abs(actuals).tolist())
+    plt.plot(range(1, 6), results)
+    # plt.xticks(range(0, 5), range(1, 6))
     plt.title("RMSE By Degree of Regression for Housing Index Only")
     plt.xlabel("Degree")
     plt.ylabel("RMSE")
     plt.show()
+
 
 
