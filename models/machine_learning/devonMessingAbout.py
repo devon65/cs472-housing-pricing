619c432a00b81e9ccb84672a60b8d989b0d350c5
Devon Howard <how.d.65@gmail.com>
2020-04-01 20:54:34 -0600

    cleaned code

--- models/machine_learning/devonMessingAbout.py
+++ models/machine_learning/devonMessingAbout.py
@@ -4,7 +4,6 @@ from data.loader import HousingDataset as hd
 import pandas as pd
 import numpy as np
 from sklearn.neighbors import KNeighborsRegressor as KNNRegressor
-from KNN import KNNClassifier as KNN, NO_WEIGHT, INVERSE_DISTANCE, CONTINUOUS, NOMINAL
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error as MSE
 
